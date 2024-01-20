b330c82e632b26082318c5aee136347b6b20f786
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-01-21 03:32:03 +0900

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-
-createApp(App).use(router).use(inject).mount("#app");
+inject();
+createApp(App).use(router).mount("#app");
