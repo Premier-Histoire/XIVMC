e15981af041580fce8c2d819ca0edda15a7c9b83
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-01-21 03:40:20 +0900

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import "mdb-vue-ui-kit/css/mdb.min.css";
-import { inject } from '@vercel/analytics';
 
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-inject();
+
 createApp(App).use(router).mount("#app");
